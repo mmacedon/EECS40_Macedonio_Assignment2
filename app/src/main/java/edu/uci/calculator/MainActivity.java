c004be1a6611c8603023fa59ed5e9e43dd6fadf2
Manuel <mmacedon@uci.edu>
2018-05-05 00:21:55 -0700

    "Defined and Debug Solving Algorithm"

--- app/src/main/java/edu/uci/calculator/MainActivity.java
+++ app/src/main/java/edu/uci/calculator/MainActivity.java
@@ -1,188 +1,363 @@
 package edu.uci.calculator;
 
+import android.support.v4.content.res.TypedArrayUtils;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import java.io.IOException;
 import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    char [] current_array = new char [ 15 ] ;
-    String current = "";
+    String input = "";
+    String output = "";
+    String out = "";
+    String [] valuesbuffer = new String [ 20 ];
+    char [] operationsbuffer = new char [ 20 ];
+    boolean percentflag = false;
+    char tracker = '\0';
+    char operator = 'a';
+    double ans = 0.0;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate ( Bundle savedInstanceState )
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
 
-    public void toScreen ( String current )
+
+
+    public void clearScreen ( View view )
+    {
+        TextView textView = ( TextView ) findViewById ( R.id.textView2 );
+        textView.setText( " " );
+    }
+
+    public void toScreen ( String display )
     {
         TextView textView = ( TextView ) findViewById( R.id.textView2 );
-
-        textView.setText ( current );
+        textView.setText ( display );
     }
 
     public void Append9 ( View view )
     {
-        if ( current.length() == 15 ) { return; }
-        current = current + "9";
-        toScreen( current );
+        tracker = '\0';
+        input = input + "9";
+        toScreen( input );
     }
+
     public void Append8 ( View view )
     {
-        if ( current.length() == 15 ) { return; }
-        current = current + "8";
-        toScreen( current );
+        tracker = '\0';
+        input = input + "8";
+        toScreen( input );
     }
+
     public void Append7 ( View view )
     {
-        current = current + "7";
-        toScreen( current );
+        tracker = '\0';
+        input = input + "7";
+        toScreen( input );
     }
+
     public void Append6 ( View view )
     {
-        if ( current.length() == 15 ) { return; }
-        current = current + "6";
-        toScreen( current );
+        tracker = '\0';
+        input = input + "6";
+        toScreen( input );
     }
+
     public void Append5 ( View view )
     {
-        if ( current.length() == 15 ) { return; }
-        current = current + "5";
-        toScreen( current );
+        tracker = '\0';
+        input = input + "5";
+        toScreen( input );
     }
+
     public void Append4 ( View view )
     {
-        if ( current.length() == 15 ) { return; }
-        current = current + "4";
-        toScreen( current );
+        tracker = '\0';
+        input = input + "4";
+        toScreen( input );
     }
+
     public void Append3 ( View view )
     {
-        if ( current.length() == 15 ) { return; }
-        current = current + "3";
-        toScreen( current );
+        tracker = '\0';
+        input = input + "3";
+        toScreen( input );
     }
+
     public void Append2 ( View view )
     {
-        if ( current.length() == 15 ) { return; }
-        current = current + "2";
-        toScreen( current );
+        tracker = '\0';
+        input = input + "2";
+        toScreen( input );
     }
+
     public void Append1 ( View view )
     {
-        current = current + "1";
-        toScreen( current );
+        tracker = '\0';
+        input = input + "1";
+        toScreen( input );
     }
+
     public void Append0 ( View view )
     {
-        if ( current.length() == 15 ) { return; }
-        current = current + "0";
-        toScreen( current );
+        tracker = '\0';
+        input = input + "0";
+        toScreen( input );
     }
+
+    public void Appenddecimal ( View view )
+    {
+        if ( tracker == '.' ) { return; }
+        tracker = '.';
+        input = input + ".";
+        toScreen( input );
+    }
+
     public void Appendd ( View view )
     {
-        if ( current.length() == 15 ) { return; }
-        current = current + "/";
-        toScreen( current );
+        if ( tracker == '/' || tracker == 'x' || tracker == '-' || tracker == '+' || tracker == '.' ) { return; }
+        tracker = '/';
+        input = input + "/";
+        toScreen( input );
     }
     public void Appendx ( View view )
     {
-        if ( current.length() == 15 ) { return; }
-        current = current + "x";
-        toScreen( current );
+        if ( tracker == '/' || tracker == 'x' || tracker == '-' || tracker == '+' || tracker == '.' ) { return; }
+        tracker = 'x';
+        input = input + "x";
+        toScreen( input );
     }
     public void Appends ( View view )
     {
-        if ( current.length() == 15 ) { return; }
-        current = current + "-";
-        toScreen( current );
+        if ( tracker == '/' || tracker == 'x' || tracker == '-' || tracker == '+' || tracker == '.' ) { return; }
+        tracker = '-';
+        input = input + "-";
+        toScreen( input );
     }
     public void Appenda ( View view )
     {
-        if ( current.length() == 15 ) { return; }
-        current = current + "+";
-        toScreen( current );
+        if ( tracker == '/' || tracker == 'x' || tracker == '-' || tracker == '+' || tracker == '.' ) { return; }
+        tracker = '+';
+        input = input + "+";
+        toScreen( input );
     }
 
-    public void Percent ( View view ){}
+    public void Percent ( View view ){
+        double value;
+        boolean flag = true;
+
+        for ( int i = 0; i < input.length(); i++ )
+        {
+            if ( (input.charAt ( i ) == 'x' ||
+                 input.charAt ( i ) == '/' ||
+                 input.charAt ( i ) == '+' ||
+                 input.charAt ( i ) == '-' ) &&
+                         (
+                         input.charAt ( 0 ) != 'x' ||
+                    input.charAt ( 0 ) != '/' ||
+                    input.charAt ( 0 ) != '+' ||
+                    input.charAt ( 0 ) != '-') &&
+                            ( input.charAt ( input.length() - 1 ) != 'x' ||
+                    input.charAt ( input.length() - 1 ) != '/' ||
+                    input.charAt ( input.length() - 1 ) != '+' ||
+                    input.charAt ( input.length() - 1 ) != '-')
+                    )
+            {
+                flag = false;
+            }
+        }
+
+        if ( flag )
+        {
+            value = Double.parseDouble( input );
+            value = value / 100;
+            output = Double.toString( value );
+            toScreen ( output );
+            percentflag = false;
+            tracker = '\0';
+            input = output;
+            output = "";
+            return;
+        }
+
+        percentflag = true;
+        Evaluate( view );
+    }
 
     public void Delete ( View view )
     {
-
-        int n = current.length();
-
-        if ( n == 0 ) { return; }
-
-        current_array = current.toCharArray();
-        current_array [ n ] = '\0';
-        current = current_array.toString();
-        toScreen( current );
-
+        input = input.substring( 0, input.length() - 1 );
+        tracker = '\0';
+        toScreen( input );
     }
 
     public void ClearAll ( View view )
     {
-        current = "";
-        toScreen( current );
+        input = "";
+        output = "";
+        toScreen( input );
     }
 
-    public void Evaluate ( View view ) {
-        double ans;
-        double [] n = new double [ 15 ];
-        String [] buffer = new String [ 15 ];
-        char[] track = new char[ 15 ];
+    public void Evaluate ( View view )
+    {
+        valuesbuffer = input.split ( "-|x|/|\\+" );
+        int n = 0;
+        ArrayList < Double > values;
+        ArrayList < Character > operations;
+        operations = new ArrayList < Character >();
+        values = new ArrayList < Double > ();
+        Double temp;
+        char op;
+        boolean flag = true;
 
-        if ( current.length() == 0 ) { return; }
-
-        for (int i = 0; i < current.length(); i++)
+        if ( input.charAt( 0 ) == 'x' || input.charAt( 0 ) == '-'
+                || input.charAt ( 0 ) == '+' || input.charAt( 0 ) == '/' ||
+                input.charAt( input.length() - 1 ) == 'x' || input.charAt( input.length() - 1 ) == '-'
+                || input.charAt ( input.length() - 1 ) == '+'
+                || input.charAt( input.length() - 1 ) == '/' )
         {
-            if ( current.charAt(i) == '+' )
-            {
-                track [ i ] = '+';
-            }
+            toScreen ( "INVALID SYNTAX!" );
+            return;
+        }
 
-            if ( current.charAt(i) == '-' )
+        for ( int i = 0; i < input.length(); i++ )
+        {
+            if ( (input.charAt ( i ) == 'x' ||
+                    input.charAt ( i ) == '/' ||
+                    input.charAt ( i ) == '+' ||
+                    input.charAt ( i ) == '-' ) &&
+                    (
+                            input.charAt ( 0 ) != 'x' ||
+                                    input.charAt ( 0 ) != '/' ||
+                                    input.charAt ( 0 ) != '+' ||
+                                    input.charAt ( 0 ) != '-') &&
+                    ( input.charAt ( input.length() - 1 ) != 'x' ||
+                            input.charAt ( input.length() - 1 ) != '/' ||
+                            input.charAt ( input.length() - 1 ) != '+' ||
+                            input.charAt ( input.length() - 1 ) != '-')
+                    )
             {
-                track [ i ] = '-';
+                flag = false;
             }
+        }
 
-            if ( current.charAt(i) == '/' )
+        if ( flag )
+        {
+            output = input;
+            toScreen( output );
+            return;
+        }
+
+        for ( int i = 0; i < valuesbuffer.length; i++ )
+        {
+            temp =  Double.parseDouble( valuesbuffer [ i ] );
+            values.add( temp );
+        }
+
+        for ( int i = 0; i < input.length(); i++ )
+        {
+            if ( input.charAt ( i ) == 'x' ||
+                    input.charAt ( i ) == '/' ||
+                    input.charAt ( i ) == '+' ||
+                    input.charAt ( i ) == '-' )
             {
-                track [ i ] = '/';
+                operationsbuffer [ n ] = input.charAt ( i );
+                n++;
             }
+        }
+        for ( int i = 0; i < input.length(); i++ )
+        {
+            op = operationsbuffer [ i ];
+            operations.add( op );
+        }
 
-            if ( current.charAt(i) == 'x' )
-            {
-                track [ i ] = 'x';
+        n = 0;
+        while ( n < operations.size() )
+        {
+            if ( operations.get( n ) == 'x' ) {
+                operator = 'x';
             }
-
+            else if ( operations.get( n ) == '/') {
+                operator = '/';
+            }
             else
             {
-                track [ i ] = '0';
+                n++;
+                continue;
             }
+
+            if ( operator == 'x' )
+            {
+                temp = values.get ( n ) * values.get( n + 1 );
+                values.set ( n , temp );
+                values.remove( n + 1 );
+                operations.remove( n );
+            }
+            if ( operator == '/' )
+            {
+                temp = values.get ( n ) / values.get( n + 1 );
+                values.set ( n , temp );
+                values.remove( n + 1 );
+                operations.remove( n );
+            }
+            n = 0;
         }
 
-        //^^^^^^^^^^^^^^^^^^Good up there^^^^^^^^^^^^^^^
-
-        buffer = current.split ( "[+-x/]+");
-        for ( int i = 0; i < buffer.length; i++ )
-            toScreen( buffer[ i ] );
-
-        //current = buffer.toString();
-        //toScreen( current );
-        current_array = current.toCharArray();
-
-
-        for ( int i = 0; i < buffer.length; i++ )
+        n = 0;
+        while( n < operations.size() )
         {
-            n [ i ] = (double) current_array [ i ];
+            if ( operations.get( n ).equals('+') ) {
+                operator = '+';
+            }
+            else if ( operations.get( n ).equals('-') ) {
+                operator = '-';
+            }
+            else
+            {
+                n++;
+                continue;
+            }
+
+            if ( operator == '+' )
+            {
+                temp = values.get ( n ) + values.get( n + 1 );
+                values.set ( n , temp );
+                values.remove( n + 1 );
+                operations.remove( n );
+            }
+            if ( operator == '-' )
+            {
+                temp = values.get ( n ) - values.get( n + 1 );
+                values.set ( n , temp );
+                values.remove( n + 1 );
+                operations.remove( n );
+            }
+            n = 0;
         }
 
-        current = n.toString();
-       // toScreen( current );
+        n = 0;
+        ans = values.get(n);
+        if ( percentflag )
+        {
+            ans = ans / 100;
+        }
+
+        output = Double.toString( ans );
+        percentflag = false;
+        toScreen( output );
+        tracker = '\0';
+        input = "";
+        output = "";
     }
 }
